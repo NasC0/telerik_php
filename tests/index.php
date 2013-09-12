b672f2bb6cd03d4df1cd33054bda060999676f96
NasC0 <Spirital@gmail.com>
2013-09-12 18:45:01 +0300

    Fresht start

---
+++ tests/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Kickass
+ * Date: 13-9-12
+ * Time: 18:30
+ */
+$myVar['username'] = 'vance';
+echo $myVar['username'];
+echo 'Hello world!';
